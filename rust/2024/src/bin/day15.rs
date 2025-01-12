bf6b6bf3170dd546aeef6d08185f460307c4bffe
ndunnett <nathandunnett@me.com>
2025-01-12 13:04:26 +0000

    optimise 2024 day 15

--- rust/2024/src/bin/day15.rs
+++ rust/2024/src/bin/day15.rs
@@ -1,11 +1,11 @@
 #[derive(Clone, Copy, Hash, PartialEq, Eq)]
 struct Point {
-    x: i32,
-    y: i32,
+    x: u8,
+    y: u8,
 }
 
 impl Point {
-    fn new(x: i32, y: i32) -> Self {
+    fn new(x: u8, y: u8) -> Self {
         Self { x, y }
     }
 }
@@ -45,6 +45,7 @@ impl Direction {
 
 #[derive(Clone, Copy, Hash, PartialEq, Eq)]
 enum Tile {
+    Empty,
     Wall,
     Box,
     BoxLeft,
@@ -52,110 +53,147 @@ enum Tile {
 }
 
 #[derive(Clone)]
+struct Map {
+    data: [[Tile; 100]; 50],
+}
+
+impl Map {
+    fn new() -> Self {
+        Self {
+            data: [[Tile::Empty; 100]; 50],
+        }
+    }
+
+    fn get(&self, p: &Point) -> Tile {
+        self.data[p.y as usize][p.x as usize]
+    }
+
+    fn insert(&mut self, p: &Point, t: Tile) {
+        self.data[p.y as usize][p.x as usize] = t;
+    }
+}
+
+#[derive(Clone)]
 struct Solution {
-    map: FxHashMap<Point, Tile>,
-    robot: Point,
+    map: Map,
+    start: Point,
     moves: Vec<Direction>,
 }
 
 impl Solution {
-    fn solve(&mut self) -> i32 {
-        'outer: for direction in &self.moves {
-            let robot_next = direction.move_point(&self.robot);
+    fn solve(&mut self) -> usize {
+        let mut position = self.start;
 
-            if !self.map.contains_key(&robot_next) {
-                self.robot = robot_next;
-                continue;
+        self.moves.iter().for_each(|direction| {
+            let next_position = direction.move_point(&position);
+
+            // if the next tile is empty, we can move without consideration
+            if self.map.get(&next_position) == Tile::Empty {
+                position = next_position;
+                return;
             }
 
-            let mut queue = vec![robot_next];
-            let mut moves = Vec::new();
+            let mut pushes = Vec::with_capacity(32);
+            let mut queue = Vec::with_capacity(10);
+            queue.push(next_position);
 
+            // macro to conveniently add the next position to queue and push the current position to the vec of moves
+            macro_rules! queue {
+                ($($p:expr),+) => {$({
+                    let next = direction.move_point(&$p);
+                    queue.push(next);
+                    pushes.push($p);
+                })+};
+            }
+
+            // use DFS to find all boxes that will be pushed on the given move
             while let Some(p) = queue.pop() {
                 match (direction, self.map.get(&p)) {
-                    (_, Some(Tile::Wall)) => {
-                        continue 'outer;
+                    // if we reach a wall, the move isn't possible
+                    (_, Tile::Wall) => return,
+                    // when pushing north/south and we reach a big box, we need to push both sides
+                    (Direction::N | Direction::S, Tile::BoxLeft) => {
+                        // queue up next moves for the current position (left side of box) and adjacent position to the east (right side of box)
+                        queue!(p, Direction::E.move_point(&p));
                     }
-                    (Direction::N | Direction::S, Some(Tile::BoxLeft)) => {
-                        let right = Direction::E.move_point(&p);
-                        queue.push(direction.move_point(&p));
-                        queue.push(direction.move_point(&right));
-                        moves.push(p);
-                        moves.push(right);
+                    (Direction::N | Direction::S, Tile::BoxRight) => {
+                        // queue up next moves for the current position (right side of box) and adjacent position to the west (left side of box)
+                        queue!(p, Direction::W.move_point(&p));
                     }
-                    (Direction::N | Direction::S, Some(Tile::BoxRight)) => {
-                        let left = Direction::W.move_point(&p);
-                        queue.push(direction.move_point(&p));
-                        queue.push(direction.move_point(&left));
-                        moves.push(p);
-                        moves.push(left);
-                    }
-                    (Direction::E | Direction::W, Some(Tile::BoxLeft | Tile::BoxRight)) => {
-                        let front = direction.move_point(&p);
-                        queue.push(direction.move_point(&front));
-                        moves.push(p);
-                        moves.push(front);
-                    }
-                    (_, Some(Tile::Box)) => {
-                        queue.push(direction.move_point(&p));
-                        moves.push(p);
+                    // all other pushes can be handled individually
+                    (_, Tile::Box | Tile::BoxLeft | Tile::BoxRight) => {
+                        queue!(p);
                     }
                     _ => {}
                 }
             }
 
-            if !moves.is_empty() {
-                let moved = moves
-                    .iter()
-                    .filter_map(|p| Some((direction.move_point(p), self.map.remove(p)?)))
-                    .collect::<Vec<_>>();
+            // pop all tiles to be pushed from the map and collect them with new positions
+            let pushed = pushes
+                .iter()
+                .filter_map(|from| {
+                    let tile = self.map.get(from);
 
-                for (next, tile) in moved {
-                    self.map.insert(next, tile);
-                }
+                    if tile != Tile::Empty {
+                        self.map.insert(from, Tile::Empty);
+                        Some((direction.move_point(from), tile))
+                    } else {
+                        None
+                    }
+                })
+                .collect::<Vec<_>>();
+
+            // insert collected tiles back into the map in the new position
+            for (to, tile) in pushed {
+                self.map.insert(&to, tile);
             }
 
-            if !self.map.contains_key(&robot_next) {
-                self.robot = robot_next;
-            }
-        }
+            // finally move the robot
+            position = next_position;
+        });
 
         self.map
+            .data
             .iter()
-            .filter_map(|(p, &tile)| {
-                if tile == Tile::Box || tile == Tile::BoxLeft {
-                    Some(p)
-                } else {
-                    None
-                }
+            .enumerate()
+            .flat_map(|(y, row)| {
+                row.iter().enumerate().filter_map(move |(x, &tile)| {
+                    if tile == Tile::Box || tile == Tile::BoxLeft {
+                        Some(x + y * 100)
+                    } else {
+                        None
+                    }
+                })
             })
-            .fold(0, |acc, p| acc + p.x + p.y * 100)
+            .sum()
     }
 
     fn expanded(&self) -> Self {
-        Self {
-            map: self
-                .map
-                .par_iter()
-                .flat_map(|(p, tile)| match tile {
-                    Tile::Wall => {
-                        vec![
-                            (Point::new(p.x * 2, p.y), Tile::Wall),
-                            (Point::new(p.x * 2 + 1, p.y), Tile::Wall),
-                        ]
-                    }
-                    Tile::Box => {
-                        vec![
-                            (Point::new(p.x * 2, p.y), Tile::BoxLeft),
-                            (Point::new(p.x * 2 + 1, p.y), Tile::BoxRight),
-                        ]
-                    }
+        let mut new = self.clone();
+
+        for row in new.map.data.iter_mut() {
+            for (x, tile) in (*row)
+                .into_iter()
+                .flat_map(|tile| match tile {
+                    // double the width of walls and empty tiles and transform boxes into big boxes:
+                    // '.' => '..'
+                    // '#' => '##'
+                    // 'O' => '[]'
+                    Tile::Empty => [Tile::Empty; 2].into_iter(),
+                    Tile::Wall => [Tile::Wall; 2].into_iter(),
+                    Tile::Box => [Tile::BoxLeft, Tile::BoxRight].into_iter(),
                     _ => unreachable!(),
                 })
-                .collect(),
-            robot: Point::new(self.robot.x * 2, self.robot.y),
-            moves: self.moves.clone(),
+                .take(100)
+                .enumerate()
+            {
+                row[x] = tile;
+            }
         }
+
+        // starting position is twice as wide due to expansion
+        new.start.x *= 2;
+        new
     }
 }
 
@@ -165,25 +203,35 @@ impl Solver for Solution {
             .split_once("\n\n")
             .ok_or(anyhow!("failed to split input"))?;
 
-        let mut map = FxHashMap::default();
-        let mut robot = None;
+        let mut map = Map::new();
+        let mut start = None;
+        let mut p = Point::new(0, 0);
 
-        for (line, y) in map_chunk.lines().zip(0..i32::MAX) {
-            for (b, x) in line.bytes().zip(0..i32::MAX) {
-                match b {
-                    b'#' => {
-                        map.insert(Point::new(x, y), Tile::Wall);
-                    }
-                    b'O' => {
-                        map.insert(Point::new(x, y), Tile::Box);
-                    }
-                    b'@' => robot = Some(Point::new(x, y)),
-                    _ => {}
+        for b in map_chunk.bytes() {
+            match b {
+                b'\n' => {
+                    p.x = 0;
+                    p.y += 1;
+                }
+                b'#' => {
+                    map.insert(&p, Tile::Wall);
+                    p.x += 1;
+                }
+                b'O' => {
+                    map.insert(&p, Tile::Box);
+                    p.x += 1;
+                }
+                b'@' => {
+                    start = Some(p);
+                    p.x += 1;
+                }
+                _ => {
+                    p.x += 1;
                 }
             }
         }
 
-        let robot = robot.ok_or(anyhow!("failed to find robot"))?;
+        let start = start.ok_or(anyhow!("failed to find robot"))?;
 
         let moves = moves_chunk
             .bytes()
@@ -191,7 +239,7 @@ impl Solver for Solution {
             .map(Direction::try_from)
             .collect::<Anyhow<Vec<_>>>()?;
 
-        Ok(Self { map, robot, moves })
+        Ok(Self { map, start, moves })
     }
 
     fn part1(&mut self) -> Anyhow<impl fmt::Display> {
@@ -199,7 +247,7 @@ impl Solver for Solution {
     }
 
     fn part2(&mut self) -> Anyhow<impl fmt::Display> {
-        Ok(self.clone().expanded().solve())
+        Ok(self.expanded().solve())
     }
 }
 
