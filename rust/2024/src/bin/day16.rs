c8e02dab814f5941a24998a75a91c8ea8b4d11ef
ndunnett <nathandunnett@me.com>
2025-01-13 14:15:11 +0000

    optimise 2024 day 16

--- rust/2024/src/bin/day16.rs
+++ rust/2024/src/bin/day16.rs
@@ -1,18 +1,18 @@
 use std::{cmp::Ordering, collections::BinaryHeap};
 
-#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 struct Point {
-    x: usize,
-    y: usize,
+    x: u8,
+    y: u8,
 }
 
 impl Point {
-    fn new(x: usize, y: usize) -> Self {
+    fn new(x: u8, y: u8) -> Self {
         Self { x, y }
     }
 }
 
-#[derive(Clone, Copy, Hash, PartialEq, Eq)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum Direction {
     N,
     E,
@@ -21,17 +21,28 @@ enum Direction {
 }
 
 impl Direction {
-    const ALL: [Self; 4] = [Self::N, Self::E, Self::S, Self::W];
+    const REVERSE: [usize; 4] = [2, 3, 0, 1];
 
-    fn next_move(&self, p: Point, d: Self, size: usize) -> Option<(Point, Self)> {
-        match (d, self) {
-            (Self::N, Self::S) | (Self::S, Self::N) => None,
-            (Self::E, Self::W) | (Self::W, Self::E) => None,
-            (_, Self::N) if p.y > 0 => Some((Point::new(p.x, p.y - 1), *self)),
-            (_, Self::E) if p.x < size - 1 => Some((Point::new(p.x + 1, p.y), *self)),
-            (_, Self::S) if p.y < size - 1 => Some((Point::new(p.x, p.y + 1), *self)),
-            (_, Self::W) if p.x > 0 => Some((Point::new(p.x - 1, p.y), *self)),
-            _ => None,
+    const NEXT: [[Self; 3]; 4] = [
+        [Self::N, Self::E, Self::W],
+        [Self::N, Self::E, Self::S],
+        [Self::E, Self::S, Self::W],
+        [Self::N, Self::S, Self::W],
+    ];
+
+    const TURN: [[Self; 2]; 4] = [
+        [Self::E, Self::W],
+        [Self::N, Self::S],
+        [Self::E, Self::W],
+        [Self::N, Self::S],
+    ];
+
+    fn next_move(&self, p: &Point) -> Point {
+        match self {
+            Self::N => Point::new(p.x, p.y - 1),
+            Self::E => Point::new(p.x + 1, p.y),
+            Self::S => Point::new(p.x, p.y + 1),
+            Self::W => Point::new(p.x - 1, p.y),
         }
     }
 }
@@ -40,8 +51,7 @@ impl Direction {
 struct State {
     p: Point,
     d: Direction,
-    path: Vec<Point>,
-    score: usize,
+    score: u32,
 }
 
 impl Ord for State {
@@ -56,73 +66,146 @@ impl PartialOrd for State {
     }
 }
 
+#[derive(Clone, Copy)]
+struct Set {
+    data: [[u64; 3]; 141],
+}
+
+impl Set {
+    fn new() -> Self {
+        Self {
+            data: [[0; 3]; 141],
+        }
+    }
+
+    fn insert(&mut self, p: &Point) -> bool {
+        let i = p.x % 64;
+        let j = (p.x - i) as usize / 64;
+        let n = 1 << i;
+
+        if self.data[p.y as usize][j] & n == 0 {
+            self.data[p.y as usize][j] |= n;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn contains(&self, p: &Point) -> bool {
+        let i = p.x % 64;
+        self.data[p.y as usize][(p.x - i) as usize / 64] & (1 << i) != 0
+    }
+
+    fn len(&self) -> usize {
+        self.data
+            .iter()
+            .flat_map(|row| row.iter().map(|chunk| chunk.count_ones() as usize))
+            .sum()
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Map<T: Clone + Copy> {
+    data: [[T; 141]; 141],
+}
+
+impl<T: Clone + Copy> Map<T> {
+    fn new(default: T) -> Self {
+        Self {
+            data: [[default; 141]; 141],
+        }
+    }
+
+    fn get(&self, p: &Point) -> T {
+        self.data[p.y as usize][p.x as usize]
+    }
+
+    fn get_mut(&mut self, p: &Point) -> &mut T {
+        &mut self.data[p.y as usize][p.x as usize]
+    }
+}
+
 #[derive(Clone)]
 struct Solution {
-    best_score: usize,
-    best_tiles: usize,
+    start: Point,
+    end: Point,
+    scores: [Map<u32>; 4],
+    best_score: u32,
 }
 
 impl Solver for Solution {
     fn new(input: &str) -> Anyhow<Self> {
-        let maze = input.bytes().collect::<Vec<_>>();
-        let size = input.lines().count();
+        let mut maze = Set::new();
+        let mut p = Point::new(0, 0);
+
+        for b in input.bytes() {
+            match b {
+                b'\n' => {
+                    p.x = 0;
+                    p.y += 1;
+                }
+                b'#' => {
+                    maze.insert(&p);
+                    p.x += 1;
+                }
+                _ => {
+                    p.x += 1;
+                }
+            }
+        }
+
+        let size = input.lines().count() as u8;
         let start = Point::new(1, size - 2);
         let end = Point::new(size - 2, 1);
 
-        let mut queue = BinaryHeap::from_iter([State {
+        // use Dijkstra's algorithm to find best paths
+        let mut queue = BinaryHeap::with_capacity(150);
+        let mut seen = [Set::new(); 4];
+        let mut scores = [Map::new(u32::MAX); 4];
+
+        queue.push(State {
             p: start,
             d: Direction::E,
-            path: vec![start],
             score: 0,
-        }]);
+        });
 
-        let mut seen = FxHashMap::default();
-        let mut best_score = None;
-        let mut best_paths = Vec::new();
+        while let Some(State { p, d, score }) = queue.pop() {
+            let best_score = scores[d as usize].get_mut(&p);
+            *best_score = (*best_score).min(score);
 
-        while let Some(State { p, d, path, score }) = queue.pop() {
+            if !seen[d as usize].insert(&p) {
+                continue;
+            }
+
             if p == end {
-                if best_score.is_none_or(|s| s > score) {
-                    best_score = Some(score);
-                    best_paths.clear();
-                    best_paths.push(path.clone());
-                } else if best_score == Some(score) {
-                    best_paths.push(path.clone());
-                } else {
-                    break;
-                }
+                return Ok(Self {
+                    start,
+                    end,
+                    scores,
+                    best_score: score,
+                });
             }
 
-            seen.insert((p, d), score);
+            let next_p = d.next_move(&p);
 
-            for (next_p, next_d) in Direction::ALL
-                .iter()
-                .filter_map(|next_d| next_d.next_move(p, d, size))
-                .filter(|(p, _)| maze[p.x + p.y * (size + 1)] != b'#')
-            {
-                let next_score = if next_d == d { score + 1 } else { score + 1001 };
+            if !maze.contains(&next_p) {
+                queue.push(State {
+                    p: next_p,
+                    d,
+                    score: score + 1,
+                });
+            }
 
-                if seen.get(&(next_p, next_d)).is_none_or(|&s| s > next_score) {
-                    queue.push(State {
-                        p: next_p,
-                        d: next_d,
-                        path: [&path[..], &[next_p]].concat(),
-                        score: next_score,
-                    });
-                }
+            for next_d in Direction::TURN[d as usize] {
+                queue.push(State {
+                    p,
+                    d: next_d,
+                    score: score + 1000,
+                });
             }
         }
 
-        let mut best_tiles: FxHashSet<Point> = FxHashSet::default();
-
-        for path in best_paths {
-            best_tiles.extend(path);
-        }
-
-        Ok(Self {
-            best_score: best_score.ok_or(anyhow!("failed to find path"))?,
-            best_tiles: best_tiles.len(),
-        })
+        Err(anyhow!("failed to find path"))
     }
 
     fn part1(&mut self) -> Anyhow<impl fmt::Display> {
@@ -130,7 +213,48 @@ impl Solver for Solution {
     }
 
     fn part2(&mut self) -> Anyhow<impl fmt::Display> {
-        Ok(self.best_tiles)
+        // reverse walk scores from the end to the start with DFS, following descending scores to find the best tiles
+        let mut best_tiles = Set::new();
+        best_tiles.insert(&self.start);
+        best_tiles.insert(&self.end);
+
+        let mut queue = Vec::with_capacity(8);
+
+        queue.push(State {
+            p: self.end,
+            d: Direction::W,
+            score: self.best_score,
+        });
+
+        queue.push(State {
+            p: self.end,
+            d: Direction::S,
+            score: self.best_score,
+        });
+
+        while let Some(State { p, d, score }) = queue.pop() {
+            for next_d in Direction::NEXT[d as usize] {
+                let next_p = next_d.next_move(&p);
+
+                if next_p == self.start {
+                    continue;
+                }
+
+                let next_score = self.scores[Direction::REVERSE[next_d as usize]].get(&next_p);
+
+                if next_score < score {
+                    best_tiles.insert(&next_p);
+
+                    queue.push(State {
+                        p: next_p,
+                        d: next_d,
+                        score: next_score,
+                    });
+                }
+            }
+        }
+
+        Ok(best_tiles.len())
     }
 }
 
