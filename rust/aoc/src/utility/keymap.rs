cc04b6e575ca2cb3353da3a4d10303c83a87ac61
ndunnett <nathandunnett@me.com>
2025-11-12 09:52:31 +0000

    clean up KeyMap and make key type generic

--- rust/aoc/src/utility/keymap.rs
+++ rust/aoc/src/utility/keymap.rs
@@ -1,52 +1,80 @@
-use std::mem::MaybeUninit;
+use std::{marker::PhantomData, mem::MaybeUninit, ops::Index};
 
 /// Work in progress minimal implementation of a high performance key-value collection. Very unsafe, no bounds checks.
-pub struct KeyMap<T, const N: usize, const B: usize = { N.div_ceil(usize::BITS as usize) }> {
-    data: Box<[MaybeUninit<T>; N]>,
-    bitset: [usize; B],
+pub struct KeyMap<K, V, const CAPACITY: usize>
+where
+    [(); CAPACITY.div_ceil(usize::BITS as usize)]: Sized,
+{
+    data: Box<[MaybeUninit<V>; CAPACITY]>,
+    bitset: [usize; CAPACITY.div_ceil(usize::BITS as usize)],
+    marker: PhantomData<K>,
 }
 
-impl<T: Copy, const N: usize, const B: usize> KeyMap<T, N, B> {
-    #[inline(always)]
-    pub fn new() -> Self {
-        Self {
-            data: Box::new([const { MaybeUninit::uninit() }; _]),
-            bitset: [0; _],
+macro_rules! keymap_impl {
+    ($($t:ty),+) => { $(
+        impl<V: Copy, const CAPACITY: usize> KeyMap<$t, V, CAPACITY>
+        where
+            [(); CAPACITY.div_ceil(usize::BITS as usize)]: Sized,
+        {
+            #[inline(always)]
+            pub fn new() -> Self {
+                Self {
+                    data: Box::new([MaybeUninit::uninit(); _]),
+                    bitset: [0; _],
+                    marker: PhantomData,
+                }
+            }
+
+            #[inline(always)]
+            const fn select_bit(key: $t) -> (usize, $t) {
+                let chunk = key as usize >> (usize::BITS.trailing_zeros() as usize);
+                let bit = key & (usize::BITS as $t - 1);
+                (chunk, bit)
+            }
+
+            #[inline(always)]
+            pub fn insert(&mut self, key: $t, value: V) {
+                unsafe { self.data.get_unchecked_mut(key as usize).write(value) };
+                let (chunk, bit) = Self::select_bit(key);
+                unsafe { *self.bitset.get_unchecked_mut(chunk) |= 1 << bit };
+            }
+
+            #[inline(always)]
+            pub fn get(&self, key: $t) -> Option<&V> {
+                if self.contains_key(key) {
+                    Some(unsafe { self.data.get_unchecked(key as usize).assume_init_ref() })
+                } else {
+                    None
+                }
+            }
+
+            #[inline(always)]
+            pub fn contains_key(&self, key: $t) -> bool {
+                let (chunk, bit) = Self::select_bit(key);
+                unsafe { (self.bitset.get_unchecked(chunk) >> bit) & 1 == 1 }
+            }
         }
-    }
 
-    #[inline(always)]
-    const fn select_bit(key: u16) -> (usize, u16) {
-        let chunk = key as usize >> (usize::BITS.trailing_zeros() as usize);
-        let bit = key & (usize::BITS as u16 - 1);
-        (chunk, bit)
-    }
-
-    #[inline(always)]
-    pub fn insert(&mut self, key: u16, value: T) {
-        unsafe { self.data.get_unchecked_mut(key as usize).write(value) };
-        let (chunk, bit) = Self::select_bit(key);
-        unsafe { *self.bitset.get_unchecked_mut(chunk) |= 1 << bit };
-    }
-
-    #[inline(always)]
-    pub fn get(&self, key: u16) -> Option<&T> {
-        if self.contains_key(key) {
-            Some(unsafe { self.data.get_unchecked(key as usize).assume_init_ref() })
-        } else {
-            None
+        impl<V: Copy, const CAPACITY: usize> Default for KeyMap<$t, V, CAPACITY>
+        where
+            [(); CAPACITY.div_ceil(usize::BITS as usize)]: Sized,
+        {
+            fn default() -> Self {
+                Self::new()
+            }
         }
-    }
 
-    #[inline(always)]
-    pub fn contains_key(&self, key: u16) -> bool {
-        let (chunk, bit) = Self::select_bit(key);
-        unsafe { (self.bitset.get_unchecked(chunk) >> bit) & 1 == 1 }
-    }
+        impl<V: Copy, const CAPACITY: usize> Index<usize> for KeyMap<$t, V, CAPACITY>
+        where
+            [(); CAPACITY.div_ceil(usize::BITS as usize)]: Sized,
+        {
+            type Output = V;
+
+            fn index(&self, index: usize) -> &Self::Output {
+                unsafe { self.data[index].assume_init_ref() }
+            }
+        }
+    )+ };
 }
 
-impl<T: Copy, const N: usize, const B: usize> Default for KeyMap<T, N, B> {
-    fn default() -> Self {
-        Self::new()
-    }
-}
+keymap_impl!(u8, u16, u32, u64, usize);
