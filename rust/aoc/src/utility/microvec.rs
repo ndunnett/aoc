2600f501920a38adca713423e77dac073937c506
ndunnett <nathandunnett@me.com>
2025-12-01 12:24:30 +0000

    add const guard to verify that LenType is valid

--- rust/aoc/src/utility/microvec.rs
+++ rust/aoc/src/utility/microvec.rs
@@ -1,19 +1,27 @@
 use std::{
+    marker::PhantomData,
     mem::MaybeUninit,
     ops::{Index, IndexMut},
+    slice::SliceIndex,
 };
 
+use crate::utility::{Guard, True};
+
 pub const trait Usizeable: Copy {
+    fn from_usize(value: usize) -> Self;
     fn as_usize(self) -> usize;
     fn increment(&mut self);
     fn decrement(&mut self);
     fn zero() -> Self;
     fn is_zero(&self) -> bool;
+    fn max_value() -> usize;
 }
 
 macro_rules! usizeable_impl {
     ($($t:ty),+) => { $(
         impl const Usizeable for $t {
+            #[inline(always)]
+            fn from_usize(value: usize) -> Self { value as Self }
             #[inline(always)]
             fn as_usize(self) -> usize { self as usize }
             #[inline(always)]
@@ -24,27 +32,41 @@ macro_rules! usizeable_impl {
             fn zero() -> Self { 0 }
             #[inline(always)]
             fn is_zero(&self) -> bool { *self == 0 }
+            #[inline(always)]
+            fn max_value() -> usize { Self::MAX as usize }
         }
+
     )+ };
 }
 
 usizeable_impl!(u8, u16, u32, u64, usize);
 
-pub struct IntoIter<T, const CAPACITY: usize, LenType: const Usizeable> {
-    vec: MicroVec<T, CAPACITY, LenType>,
-    index: usize,
+/// Ensures that `LenType` is large enough for a given `CAPACITY` at compile time.
+pub struct LenTypeBigEnough<const CAPACITY: usize, LenType: const Usizeable> {
+    marker: PhantomData<LenType>,
 }
 
-/// Work in progress minimal implementation of a high performance heapless vector.
+impl<const CAPACITY: usize, LenType: const Usizeable> True for LenTypeBigEnough<CAPACITY, LenType> where
+    Guard<{ LenType::max_value() >= CAPACITY }>: True
+{
+}
+
+/// Work in progress minimal implementation of a high performance, heapless, const-friendly vector.
 ///
 /// Should not be used if you rely on elements being dropped, due to `MicroVec<T, ...>` implementing `Copy` where `T: Copy`,
 /// and specialised `Drop` implementations being forbidden, `Drop` will never be called for `T`.
-pub struct MicroVec<T, const CAPACITY: usize, LenType: const Usizeable> {
+pub struct MicroVec<T, const CAPACITY: usize, LenType: const Usizeable>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
+{
     data: [MaybeUninit<T>; CAPACITY],
     len: LenType,
 }
 
-impl<T, const CAPACITY: usize, LenType: const Usizeable> MicroVec<T, CAPACITY, LenType> {
+impl<T, const CAPACITY: usize, LenType: const Usizeable> MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
+{
     #[inline(always)]
     pub const fn new() -> Self {
         Self {
@@ -253,8 +275,35 @@ impl<T, const CAPACITY: usize, LenType: const Usizeable> MicroVec<T, CAPACITY, L
     }
 }
 
-impl<T, const CAPACITY: usize, LenType: const Usizeable> Default
+/// Ensures that the given `MicroVec` type is large enough for a given array at compile time.
+pub struct VecBigEnough<const VEC_SIZE: usize, const ARRAY_SIZE: usize>;
+
+impl<const VEC_SIZE: usize, const ARRAY_SIZE: usize> True for VecBigEnough<VEC_SIZE, ARRAY_SIZE> where
+    Guard<{ VEC_SIZE >= ARRAY_SIZE }>: True
+{
+}
+
+impl<T: Copy, const CAPACITY: usize, LenType: const Usizeable, const N: usize> From<[T; N]>
     for MicroVec<T, CAPACITY, LenType>
+where
+    VecBigEnough<CAPACITY, N>: True,
+    LenTypeBigEnough<CAPACITY, LenType>: True,
+{
+    fn from(value: [T; N]) -> Self {
+        let mut vec = Self::new();
+
+        unsafe {
+            std::ptr::copy_nonoverlapping(value.as_ptr(), vec.as_mut_ptr(), N);
+        }
+
+        vec.len = LenType::from_usize(N);
+        vec
+    }
+}
+
+impl<T, const CAPACITY: usize, LenType: const Usizeable> Default for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     #[inline(always)]
     fn default() -> Self {
@@ -264,6 +313,8 @@ impl<T, const CAPACITY: usize, LenType: const Usizeable> Default
 
 impl<T, const CAPACITY: usize, LenType: const Usizeable> FromIterator<T>
     for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
         let mut vec = Self::new();
@@ -276,57 +327,10 @@ impl<T, const CAPACITY: usize, LenType: const Usizeable> FromIterator<T>
     }
 }
 
-impl<T, const CAPACITY: usize, LenType: const Usizeable> Index<usize>
-    for MicroVec<T, CAPACITY, LenType>
-{
-    type Output = T;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        self.get(index).expect("index out of bounds")
-    }
-}
-
-impl<T, const CAPACITY: usize, LenType: const Usizeable> IndexMut<usize>
-    for MicroVec<T, CAPACITY, LenType>
-{
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        self.get_mut(index).expect("index out of bounds")
-    }
-}
-
-impl<T, const CAPACITY: usize, LenType: const Usizeable> Index<std::ops::Range<usize>>
-    for MicroVec<T, CAPACITY, LenType>
-{
-    type Output = [T];
-
-    fn index(&self, index: std::ops::Range<usize>) -> &Self::Output {
-        if index.end < self.len() && index.start <= index.end {
-            unsafe { std::slice::from_raw_parts(self.ptr_to(index.start), index.end - index.start) }
-        } else {
-            panic!("index out of bounds");
-        }
-    }
-}
-
-impl<T, const CAPACITY: usize, LenType: const Usizeable> IndexMut<std::ops::Range<usize>>
-    for MicroVec<T, CAPACITY, LenType>
-{
-    fn index_mut(&mut self, index: std::ops::Range<usize>) -> &mut Self::Output {
-        if index.end < self.len() && index.start <= index.end {
-            unsafe {
-                std::slice::from_raw_parts_mut(
-                    self.mut_ptr_to(index.start),
-                    index.end - index.start,
-                )
-            }
-        } else {
-            panic!("index out of bounds");
-        }
-    }
-}
-
 impl<T: Clone, const CAPACITY: usize, LenType: const Usizeable> Clone
     for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     fn clone(&self) -> Self {
         let mut cloned = Self::new();
@@ -348,11 +352,23 @@ impl<T: Clone, const CAPACITY: usize, LenType: const Usizeable> Clone
 
 impl<T: Copy, const CAPACITY: usize, LenType: const Usizeable> Copy
     for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
+}
+
+pub struct IntoIter<T, const CAPACITY: usize, LenType: const Usizeable>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
+{
+    vec: MicroVec<T, CAPACITY, LenType>,
+    index: usize,
 }
 
 impl<T, const CAPACITY: usize, LenType: const Usizeable> IntoIterator
     for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     type Item = T;
     type IntoIter = IntoIter<T, CAPACITY, LenType>;
@@ -366,8 +382,9 @@ impl<T, const CAPACITY: usize, LenType: const Usizeable> IntoIterator
     }
 }
 
-impl<T, const CAPACITY: usize, LenType: const Usizeable> Iterator
-    for IntoIter<T, CAPACITY, LenType>
+impl<T, const CAPACITY: usize, LenType: const Usizeable> Iterator for IntoIter<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     type Item = T;
 
@@ -396,10 +413,29 @@ impl<T, const CAPACITY: usize, LenType: const Usizeable> Iterator
     }
 }
 
+impl<T, const CAPACITY: usize, LenType: const Usizeable> ExactSizeIterator
+    for IntoIter<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
+{
+    fn len(&self) -> usize {
+        self.vec.len() - self.index
+    }
+}
+
+impl<T, const CAPACITY: usize, LenType: const Usizeable> std::iter::FusedIterator
+    for IntoIter<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
+{
+}
+
 // `Deref` into slices
 
 impl<T, const CAPACITY: usize, LenType: const Usizeable> std::ops::Deref
     for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     type Target = [T];
 
@@ -411,6 +447,8 @@ impl<T, const CAPACITY: usize, LenType: const Usizeable> std::ops::Deref
 
 impl<T, const CAPACITY: usize, LenType: const Usizeable> std::ops::DerefMut
     for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     #[inline(always)]
     fn deref_mut(&mut self) -> &mut [T] {
@@ -420,8 +458,32 @@ impl<T, const CAPACITY: usize, LenType: const Usizeable> std::ops::DerefMut
 
 // Blanket trait implementations, defer actual implementation to std::slice by dereferencing
 
+impl<T, const CAPACITY: usize, LenType: const Usizeable, I: SliceIndex<[T]>> Index<I>
+    for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
+{
+    type Output = I::Output;
+
+    fn index(&self, index: I) -> &Self::Output {
+        &(**self)[index]
+    }
+}
+
+impl<T, const CAPACITY: usize, LenType: const Usizeable, I: SliceIndex<[T]>> IndexMut<I>
+    for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
+{
+    fn index_mut(&mut self, index: I) -> &mut Self::Output {
+        &mut (**self)[index]
+    }
+}
+
 impl<'a, T, const CAPACITY: usize, LenType: const Usizeable> IntoIterator
     for &'a MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     type Item = &'a T;
     type IntoIter = std::slice::Iter<'a, T>;
@@ -434,6 +496,8 @@ impl<'a, T, const CAPACITY: usize, LenType: const Usizeable> IntoIterator
 
 impl<'a, T, const CAPACITY: usize, LenType: const Usizeable> IntoIterator
     for &'a mut MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     type Item = &'a mut T;
     type IntoIter = std::slice::IterMut<'a, T>;
@@ -446,6 +510,8 @@ impl<'a, T, const CAPACITY: usize, LenType: const Usizeable> IntoIterator
 
 impl<T: std::fmt::Debug, const CAPACITY: usize, LenType: const Usizeable> std::fmt::Debug
     for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         (**self).fmt(f)
@@ -454,6 +520,8 @@ impl<T: std::fmt::Debug, const CAPACITY: usize, LenType: const Usizeable> std::f
 
 impl<T: std::hash::Hash, const CAPACITY: usize, LenType: const Usizeable> std::hash::Hash
     for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     #[inline(always)]
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
@@ -463,6 +531,8 @@ impl<T: std::hash::Hash, const CAPACITY: usize, LenType: const Usizeable> std::h
 
 impl<T: PartialEq, const CAPACITY: usize, LenType: const Usizeable>
     PartialEq<MicroVec<T, CAPACITY, LenType>> for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     #[inline(always)]
     fn eq(&self, other: &MicroVec<T, CAPACITY, LenType>) -> bool {
@@ -472,6 +542,8 @@ impl<T: PartialEq, const CAPACITY: usize, LenType: const Usizeable>
 
 impl<T: PartialOrd, const CAPACITY: usize, LenType: const Usizeable>
     PartialOrd<MicroVec<T, CAPACITY, LenType>> for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     #[inline(always)]
     fn partial_cmp(&self, other: &MicroVec<T, CAPACITY, LenType>) -> Option<std::cmp::Ordering> {
@@ -479,10 +551,14 @@ impl<T: PartialOrd, const CAPACITY: usize, LenType: const Usizeable>
     }
 }
 
-impl<T: Eq, const CAPACITY: usize, LenType: const Usizeable> Eq for MicroVec<T, CAPACITY, LenType> {}
+impl<T: Eq, const CAPACITY: usize, LenType: const Usizeable> Eq for MicroVec<T, CAPACITY, LenType> where
+    LenTypeBigEnough<CAPACITY, LenType>: True
+{
+}
 
-impl<T: Ord, const CAPACITY: usize, LenType: const Usizeable> Ord
-    for MicroVec<T, CAPACITY, LenType>
+impl<T: Ord, const CAPACITY: usize, LenType: const Usizeable> Ord for MicroVec<T, CAPACITY, LenType>
+where
+    LenTypeBigEnough<CAPACITY, LenType>: True,
 {
     #[inline(always)]
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
@@ -490,24 +566,51 @@ impl<T: Ord, const CAPACITY: usize, LenType: const Usizeable> Ord
     }
 }
 
+/// Shorthand for constructing a `MicroVec` from an array literal.
+///
+/// 1. Push-type (const friendly), which constructs a new `MicroVec` and pushes elements sequentially.
+///    The first argument must be an expression wrapped in curly braces which evaluates to a valid `MicroVec`,
+///    the second argument must be an array literal (although an array won't actually be constructed).
+/// ```
+/// let vec = microvec!({ MicroVec::<u8, 10, u8>::new() } => [0; 10]);
+/// ```
+///
+/// 2. Copy-type (more performant), which constructs an array and copies it into a `MicroVec`.
+///    The first argument must be a valid type specifier of a `MicroVec`, and the second argument must be
+///    an array literal which will be constructed and then copied into a `MicroVec`. `T` must implement `Copy`.
+/// ```
+/// let vec = microvec!(MicroVec::<u8, 10, u8> => [1, 2, 3, 4, 5]);
+/// ```
 #[macro_export]
 macro_rules! microvec {
-    ( $t:expr => $( $x:expr ),* ) => {
+    ( { $t:expr } => [$( $x:expr ),*] ) => {
         {
             let mut vec = $t;
             $(vec.push($x);)*
             vec
         }
     };
-    ( $t:expr => $x:expr; $count:expr ) => {
+    ( { $t:expr } => [$x:expr; $count:expr] ) => {
         {
             let mut vec = $t;
+            let mut i = 0;
 
-            for _ in 0..$count {
+            while i < $count {
                 vec.push($x);
+                i += 1;
             }
 
             vec
+        }
+    };
+    ( $t:ty => [$( $x:expr ),*] ) => {
+        {
+            <$t>::from([$($x,)*].into())
+        }
+    };
+    ( $t:ty => [$x:expr; $count:expr] ) => {
+        {
+            <$t>::from([$x; $count].into())
         }
     };
 }
