ec29a7b04b761b7a1de0fb97740676abaef92ac7
ndunnett <nathandunnett@me.com>
2025-12-01 07:22:30 +0000

    rearrange impl out of a macro

--- rust/aoc/src/utility/microvec.rs
+++ rust/aoc/src/utility/microvec.rs
@@ -3,143 +3,168 @@ use std::{
     ops::{Index, IndexMut},
 };
 
-/// Work in progress minimal implementation of a high performance vector.
-#[derive(Clone, Copy)]
-pub struct MicroVec<T: Copy, const CAPACITY: usize, LenType: Copy> {
-    data: [MaybeUninit<T>; CAPACITY],
-    len: LenType,
+pub trait Usizeable: Copy + Default {
+    fn as_usize(self) -> usize;
+    fn increment(&mut self);
+    fn decrement(&mut self);
 }
 
-macro_rules! microvec_impl {
+macro_rules! usizeable_impl {
     ($($t:ty),+) => { $(
-        impl<T: Copy, const CAPACITY: usize> MicroVec<T, CAPACITY, $t> {
+        impl Usizeable for $t {
             #[inline(always)]
-            pub const fn new() -> Self {
-                Self {
-                    data: [MaybeUninit::uninit(); CAPACITY],
-                    len: 0,
-                }
-            }
-
+            fn as_usize(self) -> usize { self as usize }
             #[inline(always)]
-            pub fn as_slice(&self) -> &[T] {
-                unsafe { std::slice::from_raw_parts(self.data.as_ptr() as *const T, self.len as usize) }
-            }
-
+            fn increment(&mut self) { *self += 1; }
             #[inline(always)]
-            pub fn as_mut_slice(&mut self) -> &mut [T] {
-                unsafe {
-                    std::slice::from_raw_parts_mut(self.data.as_mut_ptr() as *mut T, self.len as usize)
-                }
-            }
-
-            #[inline(always)]
-            pub fn push(&mut self, value: T) {
-                let index = self.len as usize;
-                unsafe { self.data.get_unchecked_mut(index).write(value) };
-                self.len += 1;
-            }
-
-            #[inline(always)]
-            pub fn pop(&mut self) -> Option<T> {
-                if self.len > 0 {
-                    self.len -= 1;
-                    Some(unsafe { self.data.get_unchecked(self.len as usize).assume_init() })
-                } else {
-                    None
-                }
-            }
-
-            #[inline(always)]
-            pub const fn len(&self) -> usize {
-                self.len as usize
-            }
-
-            #[inline(always)]
-            pub const fn is_empty(&self) -> bool {
-                self.len == 0
-            }
-
-            #[inline(always)]
-            pub fn iter(&self) -> impl Iterator<Item = &T> {
-                self.as_slice().iter()
-            }
-
-            #[inline(always)]
-            pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> {
-                self.as_mut_slice().iter_mut()
-            }
-
-            #[inline(always)]
-            pub fn get(&mut self, index: usize) -> Option<&T> {
-                if index < self.len as usize {
-                    Some(&self[index])
-                } else {
-                    None
-                }
-            }
-
-            #[inline(always)]
-            pub fn get_mut(&mut self, index: usize) -> Option<&mut T> {
-                if index < self.len as usize {
-                    Some(&mut self[index])
-                } else {
-                    None
-                }
-            }
-        }
-
-        impl<T: Copy + std::fmt::Debug, const CAPACITY: usize> std::fmt::Debug
-            for MicroVec<T, CAPACITY, $t>
-        {
-            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                f.debug_list().entries(self.iter()).finish()
-            }
-        }
-
-        impl<T: Copy, const CAPACITY: usize> Default for MicroVec<T, CAPACITY, $t> {
-            fn default() -> Self {
-                Self::new()
-            }
-        }
-
-        impl<T: Copy, const CAPACITY: usize> FromIterator<T> for MicroVec<T, CAPACITY, $t> {
-            fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
-                let mut vec = Self::new();
-
-                for value in iter {
-                    vec.push(value);
-                }
-
-                vec
-            }
-        }
-
-        impl<T: Copy, const CAPACITY: usize> Index<usize> for MicroVec<T, CAPACITY, $t> {
-            type Output = T;
-
-            fn index(&self, index: usize) -> &Self::Output {
-                unsafe { self.data[index].assume_init_ref() }
-            }
-        }
-
-        impl<T: Copy, const CAPACITY: usize> IndexMut<usize> for MicroVec<T, CAPACITY, $t> {
-            fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-                unsafe { self.data[index].assume_init_mut() }
-            }
-        }
-
-        impl<T: Copy, const CAPACITY: usize> Index<std::ops::Range<usize>> for MicroVec<T, CAPACITY, $t> {
-            type Output = [T];
-
-            fn index(&self, index: std::ops::Range<usize>) -> &Self::Output {
-                &self.as_slice()[index]
-            }
+            fn decrement(&mut self) { *self -= 1; }
         }
     )+ };
 }
 
-microvec_impl!(u8, u16, u32, u64, usize);
+usizeable_impl!(u8, u16, u32, u64, usize);
+
+/// Work in progress minimal implementation of a high performance vector.
+#[derive(Clone, Copy)]
+pub struct MicroVec<T: Copy, const CAPACITY: usize, LenType: Usizeable> {
+    data: [MaybeUninit<T>; CAPACITY],
+    len: LenType,
+}
+
+impl<T: Copy, const CAPACITY: usize, LenType: Usizeable> MicroVec<T, CAPACITY, LenType> {
+    #[inline(always)]
+    pub const fn new() -> Self {
+        Self {
+            data: [MaybeUninit::uninit(); CAPACITY],
+            len: unsafe { MaybeUninit::zeroed().assume_init() },
+        }
+    }
+
+    #[inline(always)]
+    pub fn as_slice(&self) -> &[T] {
+        unsafe { std::slice::from_raw_parts(self.data.as_ptr() as *const T, self.len.as_usize()) }
+    }
+
+    #[inline(always)]
+    pub fn as_mut_slice(&mut self) -> &mut [T] {
+        unsafe {
+            std::slice::from_raw_parts_mut(self.data.as_mut_ptr() as *mut T, self.len.as_usize())
+        }
+    }
+
+    #[inline(always)]
+    pub fn push(&mut self, value: T) {
+        let index = self.len.as_usize();
+        unsafe { self.data.get_unchecked_mut(index).write(value) };
+        self.len.increment();
+    }
+
+    #[inline(always)]
+    pub fn pop(&mut self) -> Option<T> {
+        if !self.is_empty() {
+            self.len.decrement();
+            Some(unsafe { self.data.get_unchecked(self.len.as_usize()).assume_init() })
+        } else {
+            None
+        }
+    }
+
+    #[inline(always)]
+    pub fn len(&self) -> usize {
+        self.len.as_usize()
+    }
+
+    #[inline(always)]
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    #[inline(always)]
+    pub fn iter(&self) -> impl Iterator<Item = &T> {
+        self.as_slice().iter()
+    }
+
+    #[inline(always)]
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> {
+        self.as_mut_slice().iter_mut()
+    }
+
+    #[inline(always)]
+    pub fn get(&mut self, index: usize) -> Option<&T> {
+        if index < self.len.as_usize() {
+            Some(&self[index])
+        } else {
+            None
+        }
+    }
+
+    #[inline(always)]
+    pub fn get_mut(&mut self, index: usize) -> Option<&mut T> {
+        if index < self.len.as_usize() {
+            Some(&mut self[index])
+        } else {
+            None
+        }
+    }
+}
+
+impl<T: Copy + std::fmt::Debug, const CAPACITY: usize, LenType: Usizeable> std::fmt::Debug
+    for MicroVec<T, CAPACITY, LenType>
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_list().entries(self.iter()).finish()
+    }
+}
+
+impl<T: Copy, const CAPACITY: usize, LenType: Usizeable> Default
+    for MicroVec<T, CAPACITY, LenType>
+{
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T: Copy, const CAPACITY: usize, LenType: Usizeable> FromIterator<T>
+    for MicroVec<T, CAPACITY, LenType>
+{
+    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+        let mut vec = Self::new();
+
+        for value in iter {
+            vec.push(value);
+        }
+
+        vec
+    }
+}
+
+impl<T: Copy, const CAPACITY: usize, LenType: Usizeable> Index<usize>
+    for MicroVec<T, CAPACITY, LenType>
+{
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        unsafe { self.data[index].assume_init_ref() }
+    }
+}
+
+impl<T: Copy, const CAPACITY: usize, LenType: Usizeable> IndexMut<usize>
+    for MicroVec<T, CAPACITY, LenType>
+{
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        unsafe { self.data[index].assume_init_mut() }
+    }
+}
+
+impl<T: Copy, const CAPACITY: usize, LenType: Usizeable> Index<std::ops::Range<usize>>
+    for MicroVec<T, CAPACITY, LenType>
+{
+    type Output = [T];
+
+    fn index(&self, index: std::ops::Range<usize>) -> &Self::Output {
+        &self.as_slice()[index]
+    }
+}
 
 #[macro_export]
 macro_rules! microvec {
