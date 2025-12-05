c83949b53b3c0012cd152db028175221666f7ec3
ndunnett <nathandunnett@me.com>
2025-12-05 12:51:33 +0000

    optimise 2025 day 5

--- rust/2025/src/bin/day05.rs
+++ rust/2025/src/bin/day05.rs
@@ -1,31 +1,66 @@
+#![feature(portable_simd)]
+use std::simd::prelude::*;
+
+const LANE_SIZE: usize = 8;
+const NEWLINES: Simd<u8, LANE_SIZE> = Simd::splat(b'\n');
+
 #[derive(Clone)]
 struct Solution {
-    fresh_ranges: Vec<(u64, u64)>,
+    ranges: [[Simd<u64, LANE_SIZE>; 2]; 11],
     ingredients: Vec<u64>,
 }
 
 impl Solver for Solution {
     fn new(input: &str) -> Anyhow<Self> {
-        let (a, b) = input.split_once("\n\n").unwrap();
-        let ingredients = NumberParser::from(b).collect();
-        let fresh_ranges_raw = NumberParser::<u64>::from(a)
+        let bytes = input.as_bytes();
+
+        // Use SIMD to find the `\n\n` separator, no need to check the tail because the separator is always near the middle
+        let mut separator = 0;
+
+        while separator + LANE_SIZE <= bytes.len() {
+            let a = Simd::from_slice(&bytes[separator..separator + LANE_SIZE]).simd_eq(NEWLINES);
+            let b = a.shift_elements_left::<1>(false);
+            let c = (a & b).to_bitmask();
+
+            if c > 0 {
+                separator += c.trailing_zeros() as usize;
+                break;
+            }
+
+            separator += LANE_SIZE - 1;
+        }
+
+        let ingredients = NumberParser::from(&bytes[separator + 1..]).collect();
+
+        let mut ranges = [[Simd::splat(0); 2]; 11];
+        let mut index = 0;
+        let mut chunk = 0;
+
+        // Merge overlapping ranges during parsing
+        for (next_lower, next_upper) in NumberParser::<u64>::from(&bytes[..separator])
             .tuples::<(_, _)>()
-            .sorted_unstable();
+            .map(|(a, b)| (a, b + 1))
+            .sorted_unstable()
+        {
+            let prev_upper = ranges[chunk][1][index];
 
-        let mut fresh_ranges = Vec::with_capacity(90);
-
-        for (lower, upper) in fresh_ranges_raw {
-            if let Some((_, prev_upper)) = fresh_ranges.last_mut()
-                && lower <= *prev_upper + 1
-            {
-                *prev_upper = upper.max(*prev_upper);
+            if next_lower <= prev_upper {
+                ranges[chunk][1][index] = next_upper.max(prev_upper);
             } else {
-                fresh_ranges.push((lower, upper));
+                index += 1;
+
+                if index == LANE_SIZE {
+                    index = 0;
+                    chunk += 1;
+                }
+
+                ranges[chunk][0][index] = next_lower;
+                ranges[chunk][1][index] = next_upper;
             }
         }
 
         Ok(Self {
-            fresh_ranges,
+            ranges,
             ingredients,
         })
     }
@@ -34,19 +69,21 @@ impl Solver for Solution {
         Ok(self
             .ingredients
             .iter()
-            .filter(|&&ingredient| {
-                self.fresh_ranges
-                    .iter()
-                    .any(|&(lower, upper)| lower <= ingredient && ingredient <= upper)
+            .copied()
+            .map(Simd::splat)
+            .filter(|&lane| {
+                self.ranges.iter().any(|[lower, upper]| {
+                    lower.simd_le(lane).to_bitmask() & upper.simd_gt(lane).to_bitmask() != 0
+                })
             })
             .count())
     }
 
     fn part2(&mut self) -> Anyhow<impl fmt::Display> {
         Ok(self
-            .fresh_ranges
+            .ranges
             .iter()
-            .map(|(lower, upper)| upper - lower + 1)
+            .map(|[lower, upper]| (upper - lower).reduce_sum())
             .sum::<u64>())
     }
 }
