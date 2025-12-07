f413bea797343adea4a30c06d91db9ed0d175b2f
ndunnett <nathandunnett@me.com>
2025-12-07 01:33:48 +0000

    optimise 2025 day 6

--- rust/2025/src/bin/day06.rs
+++ rust/2025/src/bin/day06.rs
@@ -3,34 +3,31 @@ use std::simd::prelude::*;
 
 const LANE_SIZE: usize = 32;
 const NEWLINES: Simd<u8, LANE_SIZE> = Simd::splat(b'\n');
+const ASCII_ZEROES: Simd<u8, LANE_SIZE> = Simd::splat(b'0');
 
 #[derive(Clone, Copy)]
+#[repr(u8)]
 enum Operator {
-    Add,
-    Mul,
+    Add = 0,
+    Mul = 1,
 }
 
 #[derive(Clone)]
 struct Solution {
-    part1: usize,
-    part2: usize,
+    part1: u64,
+    part2: u64,
 }
 
 impl Solver for Solution {
     fn new(input: &str) -> Anyhow<Self> {
         let bytes = input.as_bytes();
-        let file_end = bytes.len();
+        let file_len = bytes.len();
 
         // Use SIMD to find the first new line
         let mut first_new_line = 0;
 
         while first_new_line + LANE_SIZE <= bytes.len() {
-            let lane = Simd::from_array(
-                bytes[first_new_line..first_new_line + LANE_SIZE]
-                    .try_into()
-                    .unwrap(),
-            );
-
+            let lane = Simd::from_slice(&bytes[first_new_line..first_new_line + LANE_SIZE]);
             let mask = lane.simd_eq(NEWLINES).to_bitmask();
 
             if mask > 0 {
@@ -42,78 +39,123 @@ impl Solver for Solution {
         }
 
         let line_len = first_new_line + 1;
-        let operator_line_start = file_end - line_len;
+        let operator_line_start = file_len - line_len;
         let operand_lines = operator_line_start / line_len;
+
+        // Precompute digit table
+        let mut digits = vec![0; operator_line_start];
+        let mut i = 0;
+
+        // Do the bulk with SIMD
+        while i + LANE_SIZE <= operator_line_start {
+            // Safety: `i` can never overrun the end of the `bytes` slice as long as the line lengths are > `LANE_SIZE`
+            let lane = unsafe { *(bytes.as_ptr().add(i) as *const Simd<u8, LANE_SIZE>) };
+            let values = lane - ASCII_ZEROES;
+
+            unsafe {
+                std::ptr::copy_nonoverlapping(
+                    values.as_array().as_ptr(),
+                    digits.as_mut_ptr().add(i),
+                    LANE_SIZE,
+                );
+            }
+
+            i += LANE_SIZE;
+        }
+
+        // Check the tail with scalar loop
+        while i < operator_line_start {
+            // Safety: `i` will never overrun `digits`
+            unsafe {
+                digits.as_mut_ptr().add(i).write(bytes[i] - b'0');
+            }
+
+            i += 1;
+        }
 
         let mut part1 = 0;
         let mut part2 = 0;
-        let mut start = 0;
-        let mut len = 1;
+        let mut column_start = 0;
+        let mut column_len = 1;
 
-        while operator_line_start + start + len < file_end {
-            let operator = match bytes[operator_line_start + start] {
+        // Iterate over each column of operations
+        while operator_line_start + column_start + column_len < file_len {
+            // First byte at `operator line + start` is always the operator
+            let operator = match bytes[operator_line_start + column_start] {
                 b'+' => Operator::Add,
                 b'*' => Operator::Mul,
                 _ => unreachable!(),
             };
 
-            while bytes[operator_line_start + start + len] == b' ' {
-                len += 1;
+            // Get the width of the column by checking for spaces after the operator
+            while bytes[operator_line_start + column_start + column_len] == b' ' {
+                column_len += 1;
             }
 
-            if bytes[operator_line_start + start + len] == b'\n' {
-                len += 1;
+            // Enforce column spacing at the end of the line
+            if bytes[operator_line_start + column_start + column_len] == b'\n' {
+                column_len += 1;
             }
 
-            let mut _part1 = match operator {
-                Operator::Add => 0,
-                Operator::Mul => 1,
-            };
-            let mut _part2 = _part1;
+            // Set accumulators to 0 for addition, 1 for multiplication
+            let mut acc1 = operator as u64;
+            let mut acc2 = acc1;
 
+            // Parse out and process operands for part 1
             for line in 0..operand_lines {
                 let mut operand = 0;
-                let i = line * line_len + start;
+                let mut seen = false;
+                let start = line * line_len + column_start;
+                let end = start + column_len;
 
-                for byte in &bytes[i..i + len] {
-                    if byte.is_ascii_digit() {
-                        operand = operand * 10 + (byte - b'0') as usize;
-                    } else if operand > 0 {
+                for i in start..end {
+                    // Safety: `i` will never overrun `digits`
+                    let digit = unsafe { *digits.as_ptr().add(i) };
+
+                    if digit < 10 {
+                        operand = operand * 10 + digit as u64;
+                        seen = true;
+                    } else if seen {
                         break;
                     }
                 }
 
                 match operator {
-                    Operator::Add => _part1 += operand,
-                    Operator::Mul => _part1 *= operand,
+                    Operator::Add => acc1 += operand,
+                    Operator::Mul => acc1 *= operand,
                 }
             }
 
-            part1 += _part1;
-
-            for col in (0..len - 1).rev() {
+            // Parse out and process operands for part 2
+            for column in (0..column_len - 1).rev() {
                 let mut operand = 0;
+                let mut seen = false;
 
                 for line in 0..operand_lines {
-                    let byte = bytes[line * line_len + start + col];
+                    let i = line * line_len + column_start + column;
 
-                    if byte.is_ascii_digit() {
-                        operand = operand * 10 + (byte - b'0') as usize;
-                    } else if operand > 0 {
+                    // Safety: `i` will never overrun `digits`
+                    let digit = unsafe { *digits.as_ptr().add(i) };
+
+                    if digit < 10 {
+                        operand = operand * 10 + digit as u64;
+                        seen = true;
+                    } else if seen {
                         break;
                     }
                 }
 
                 match operator {
-                    Operator::Add => _part2 += operand,
-                    Operator::Mul => _part2 *= operand,
+                    Operator::Add => acc2 += operand,
+                    Operator::Mul => acc2 *= operand,
                 }
             }
 
-            part2 += _part2;
-
-            start += len;
-            len = 1;
+            // Add accumulators to the final answers and increment the start index
+            part1 += acc1;
+            part2 += acc2;
+            column_start += column_len;
+            column_len = 1;
         }
 
         Ok(Self { part1, part2 })
