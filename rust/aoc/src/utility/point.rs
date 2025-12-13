b0a57b1a6c8303ab0b6e6c7a05883b5dcf055373
ndunnett <nathandunnett@me.com>
2025-12-13 17:17:14 +0000

    remove bounds checks from access methods

--- rust/aoc/src/utility/point.rs
+++ rust/aoc/src/utility/point.rs
@@ -17,12 +17,12 @@ macro_rules! access_methods {
     ($index:expr, $fn:ident, $fn_mut:ident) => {
         #[inline(always)]
         pub const fn $fn(&self) -> T {
-            self.coordinates[$index]
+            unsafe { self.get_unchecked($index) }
         }
 
         #[inline(always)]
         pub const fn $fn_mut(&mut self) -> &mut T {
-            &mut self.coordinates[$index]
+            unsafe { self.get_unchecked_mut($index) }
         }
     };
 }
@@ -63,23 +63,35 @@ impl<T: Copy> GenericPoint<3, T> {
 
 impl<const N: usize, T: Copy> GenericPoint<N, T> {
     #[inline(always)]
+    const unsafe fn get_unchecked(&self, index: usize) -> T {
+        unsafe { self.coordinates.as_ptr().add(index).read() }
+    }
+
+    #[inline(always)]
+    const unsafe fn get_unchecked_mut(&mut self, index: usize) -> &mut T {
+        unsafe { self.coordinates.as_mut_ptr().add(index).as_mut_unchecked() }
+    }
+
+    #[inline(always)]
     fn _add(&mut self, rhs: Self)
     where
         T: AddAssign,
     {
-        match N {
-            2 => {
-                self.coordinates[0] += rhs.coordinates[0];
-                self.coordinates[1] += rhs.coordinates[1];
-            }
-            3 => {
-                self.coordinates[0] += rhs.coordinates[0];
-                self.coordinates[1] += rhs.coordinates[1];
-                self.coordinates[2] += rhs.coordinates[2];
-            }
-            _ => {
-                for i in 0..N {
-                    self.coordinates[i] += rhs.coordinates[i];
+        unsafe {
+            match N {
+                2 => {
+                    *self.get_unchecked_mut(0) += rhs.get_unchecked(0);
+                    *self.get_unchecked_mut(1) += rhs.get_unchecked(1);
+                }
+                3 => {
+                    *self.get_unchecked_mut(0) += rhs.get_unchecked(0);
+                    *self.get_unchecked_mut(1) += rhs.get_unchecked(1);
+                    *self.get_unchecked_mut(2) += rhs.get_unchecked(2);
+                }
+                _ => {
+                    for i in 0..N {
+                        *self.get_unchecked_mut(i) += rhs.get_unchecked(i);
+                    }
                 }
             }
         }
@@ -90,19 +102,21 @@ impl<const N: usize, T: Copy> GenericPoint<N, T> {
     where
         T: SubAssign,
     {
-        match N {
-            2 => {
-                self.coordinates[0] -= rhs.coordinates[0];
-                self.coordinates[1] -= rhs.coordinates[1];
-            }
-            3 => {
-                self.coordinates[0] -= rhs.coordinates[0];
-                self.coordinates[1] -= rhs.coordinates[1];
-                self.coordinates[2] -= rhs.coordinates[2];
-            }
-            _ => {
-                for i in 0..N {
-                    self.coordinates[i] -= rhs.coordinates[i];
+        unsafe {
+            match N {
+                2 => {
+                    *self.get_unchecked_mut(0) -= rhs.get_unchecked(0);
+                    *self.get_unchecked_mut(1) -= rhs.get_unchecked(1);
+                }
+                3 => {
+                    *self.get_unchecked_mut(0) -= rhs.get_unchecked(0);
+                    *self.get_unchecked_mut(1) -= rhs.get_unchecked(1);
+                    *self.get_unchecked_mut(2) -= rhs.get_unchecked(2);
+                }
+                _ => {
+                    for i in 0..N {
+                        *self.get_unchecked_mut(i) -= rhs.get_unchecked(i);
+                    }
                 }
             }
         }
@@ -113,19 +127,21 @@ impl<const N: usize, T: Copy> GenericPoint<N, T> {
     where
         T: MulAssign,
     {
-        match N {
-            2 => {
-                self.coordinates[0] *= rhs.coordinates[0];
-                self.coordinates[1] *= rhs.coordinates[1];
-            }
-            3 => {
-                self.coordinates[0] *= rhs.coordinates[0];
-                self.coordinates[1] *= rhs.coordinates[1];
-                self.coordinates[2] *= rhs.coordinates[2];
-            }
-            _ => {
-                for i in 0..N {
-                    self.coordinates[i] *= rhs.coordinates[i];
+        unsafe {
+            match N {
+                2 => {
+                    *self.get_unchecked_mut(0) *= rhs.get_unchecked(0);
+                    *self.get_unchecked_mut(1) *= rhs.get_unchecked(1);
+                }
+                3 => {
+                    *self.get_unchecked_mut(0) *= rhs.get_unchecked(0);
+                    *self.get_unchecked_mut(1) *= rhs.get_unchecked(1);
+                    *self.get_unchecked_mut(2) *= rhs.get_unchecked(2);
+                }
+                _ => {
+                    for i in 0..N {
+                        *self.get_unchecked_mut(i) *= rhs.get_unchecked(i);
+                    }
                 }
             }
         }
@@ -136,19 +152,21 @@ impl<const N: usize, T: Copy> GenericPoint<N, T> {
     where
         T: DivAssign,
     {
-        match N {
-            2 => {
-                self.coordinates[0] /= rhs.coordinates[0];
-                self.coordinates[1] /= rhs.coordinates[1];
-            }
-            3 => {
-                self.coordinates[0] /= rhs.coordinates[0];
-                self.coordinates[1] /= rhs.coordinates[1];
-                self.coordinates[2] /= rhs.coordinates[2];
-            }
-            _ => {
-                for i in 0..N {
-                    self.coordinates[i] /= rhs.coordinates[i];
+        unsafe {
+            match N {
+                2 => {
+                    *self.get_unchecked_mut(0) /= rhs.get_unchecked(0);
+                    *self.get_unchecked_mut(1) /= rhs.get_unchecked(1);
+                }
+                3 => {
+                    *self.get_unchecked_mut(0) /= rhs.get_unchecked(0);
+                    *self.get_unchecked_mut(1) /= rhs.get_unchecked(1);
+                    *self.get_unchecked_mut(2) /= rhs.get_unchecked(2);
+                }
+                _ => {
+                    for i in 0..N {
+                        *self.get_unchecked_mut(i) /= rhs.get_unchecked(i);
+                    }
                 }
             }
         }
@@ -167,10 +185,10 @@ impl<const N: usize, T: Copy> GenericPoint<N, T> {
     {
         match N {
             2 => {
-                let mut ax = U::from(self.coordinates[0]);
-                let mut ay = U::from(self.coordinates[1]);
-                let mut bx = U::from(rhs.coordinates[0]);
-                let mut by = U::from(rhs.coordinates[1]);
+                let mut ax = U::from(unsafe { self.get_unchecked(0) });
+                let mut ay = U::from(unsafe { self.get_unchecked(1) });
+                let mut bx = U::from(unsafe { rhs.get_unchecked(0) });
+                let mut by = U::from(unsafe { rhs.get_unchecked(1) });
 
                 if bx > ax {
                     std::mem::swap(&mut ax, &mut bx);
@@ -186,12 +204,12 @@ impl<const N: usize, T: Copy> GenericPoint<N, T> {
                 dx * dx + dy * dy
             }
             3 => {
-                let mut ax = U::from(self.coordinates[0]);
-                let mut ay = U::from(self.coordinates[1]);
-                let mut az = U::from(self.coordinates[2]);
-                let mut bx = U::from(rhs.coordinates[0]);
-                let mut by = U::from(rhs.coordinates[1]);
-                let mut bz = U::from(rhs.coordinates[2]);
+                let mut ax = U::from(unsafe { self.get_unchecked(0) });
+                let mut ay = U::from(unsafe { self.get_unchecked(1) });
+                let mut az = U::from(unsafe { self.get_unchecked(2) });
+                let mut bx = U::from(unsafe { rhs.get_unchecked(0) });
+                let mut by = U::from(unsafe { rhs.get_unchecked(1) });
+                let mut bz = U::from(unsafe { rhs.get_unchecked(2) });
 
                 if bx > ax {
                     std::mem::swap(&mut ax, &mut bx);
@@ -215,8 +233,8 @@ impl<const N: usize, T: Copy> GenericPoint<N, T> {
                 let mut result = U::from(0);
 
                 for i in 0..N {
-                    let mut a = U::from(self.coordinates[i]);
-                    let mut b = U::from(rhs.coordinates[i]);
+                    let mut a = U::from(unsafe { self.get_unchecked(i) });
+                    let mut b = U::from(unsafe { rhs.get_unchecked(i) });
 
                     if b > a {
                         std::mem::swap(&mut a, &mut b);
@@ -236,19 +254,21 @@ impl<const N: usize, T: Copy> GenericPoint<N, T> {
     where
         T: MulAssign,
     {
-        match N {
-            2 => {
-                self.coordinates[0] *= rhs;
-                self.coordinates[1] *= rhs;
-            }
-            3 => {
-                self.coordinates[0] *= rhs;
-                self.coordinates[1] *= rhs;
-                self.coordinates[2] *= rhs;
-            }
-            _ => {
-                for i in 0..N {
-                    self.coordinates[i] *= rhs;
+        unsafe {
+            match N {
+                2 => {
+                    *self.get_unchecked_mut(0) *= rhs;
+                    *self.get_unchecked_mut(1) *= rhs;
+                }
+                3 => {
+                    *self.get_unchecked_mut(0) *= rhs;
+                    *self.get_unchecked_mut(1) *= rhs;
+                    *self.get_unchecked_mut(2) *= rhs;
+                }
+                _ => {
+                    for i in 0..N {
+                        *self.get_unchecked_mut(i) *= rhs;
+                    }
                 }
             }
         }
@@ -314,5 +334,21 @@ impl<const N: usize, T: Copy + DivAssign> Div for GenericPoint<N, T> {
 impl<const N: usize, T: Copy + DivAssign> DivAssign for GenericPoint<N, T> {
     fn div_assign(&mut self, rhs: Self) {
         self._div(rhs);
+    }
+}
+
+impl<T> From<(T, T)> for GenericPoint<2, T> {
+    fn from(value: (T, T)) -> Self {
+        Self {
+            coordinates: [value.0, value.1],
+        }
+    }
+}
+
+impl<T> From<(T, T, T)> for GenericPoint<3, T> {
+    fn from(value: (T, T, T)) -> Self {
+        Self {
+            coordinates: [value.0, value.1, value.2],
+        }
     }
 }
